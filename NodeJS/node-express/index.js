4f0e350ef389882a6f21452d6358f64f2b74d0fe
tomas costanzo <tomascostanzo@hotmail.com>
2020-04-26 19:02:37 -0400

    exercises

---
+++ NodeJS/node-express/index.js
@@ -0,0 +1,45 @@
+const express = require("express"),
+  http = require("http");
+
+const hostname = "localhost";
+const port = 3000;
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const dishRouter = require("./routes/dishRouter");
+
+const app = express();
+
+app.use(morgan("dev"));
+app.use(bodyParser.json());
+app.use("/dishes", dishRouter);
+
+app.use(express.static(__dirname + "/public"));
+
+app.get("/dishes/:dishId", (req, res, next) => {
+  res.end("Will send details of the dish: " + req.params.dishId + " to you!");
+});
+
+app.post("/dishes/:dishId", (req, res, next) => {
+  res.statusCode = 403;
+  res.end("POST operation not supported on /dishes/" + req.params.dishId);
+});
+
+app.put("/dishes/:dishId", (req, res, next) => {
+  res.write("Updating the dish: " + req.params.dishId + "\n");
+  res.end(
+    "Will update the dish: " +
+      req.body.name +
+      " with details: " +
+      req.body.description
+  );
+});
+
+app.delete("/dishes/:dishId", (req, res, next) => {
+  res.end("Deleting dish: " + req.params.dishId);
+});
+
+const server = http.createServer(app);
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
