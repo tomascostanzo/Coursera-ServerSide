b002011ce0865b85b804dae47d27f36d7231bc14
tomas costanzo <tomascostanzo@hotmail.com>
2020-05-04 22:07:17 -0400

    Update files

--- NodeJS/assignement1/index.js
+++ NodeJS/assignement1/index.js
@@ -7,7 +7,7 @@ const morgan = require("morgan");
 const bodyParser = require("body-parser");
 const dishRouter = require("./routes/dishRouter");
 const promoRouter = require("./routes/promoRouter");
-const leaderRouter = require("./routes/leaderRouter");
+const leadeRouter = require("./routes/leaderRouter");
 
 const app = express();
 
@@ -15,7 +15,7 @@ app.use(morgan("dev"));
 app.use(bodyParser.json());
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
-app.use("/leaders", leaderRouter);
+app.use("/leaders", leadeRouter);
 
 app.use(express.static(__dirname + "/public"));
 
